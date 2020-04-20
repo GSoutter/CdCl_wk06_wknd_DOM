44508e9677a24859a8786eadbc98980f406743b4
Gordon Soutter <gordon.soutter@outlook.com>
2020-04-20 09:17:13 +0100

    Refactors code.

--- js/app.js
+++ js/app.js
@@ -9,40 +9,21 @@ document.addEventListener('DOMContentLoaded', ()=>{
   const clearnBtn = document.querySelector('#btn-clear-table');
   clearnBtn.addEventListener('click', clearTable);
 
-
-
-
 });
 
 
-const formHandler = function () {
+
+const testClicker = function() {
+  console.log('It has been clicked');
+};
+
+const formHandler = function (event) {
   event.preventDefault();
   console.log('form submitted');
 
-  const name = document.createElement('td');
-  const subgroup = document.createElement('td');
-  const species = document.createElement('td');
-  const country = document.createElement('td');
-  const risk = document.createElement('td');
-  const population = document.createElement('td');
+  newRow = populateNewRow(event)
 
-  const newRow = document.createElement('tr');
-
-  name.textContent = event.target.name.value;
-  subgroup.textContent = event.target.subgroup.value;
-  species.textContent = event.target.species.value;
-  country.textContent = event.target.country.value;
-  risk.textContent = event.target.risk.value;
-  population.textContent = event.target.population.value;
-
-  newRow.appendChild(name);
-  newRow.appendChild(subgroup);
-  newRow.appendChild(species);
-  newRow.appendChild(country);
-  newRow.appendChild(risk);
-  newRow.appendChild(population);
-
-  table = document.querySelector('#animal-table');
+  table = document.querySelector('#table-body');
 
   table.appendChild(newRow);
 
@@ -53,9 +34,36 @@ const formHandler = function () {
 const clearTable = function() {
   event.preventDefault();
   console.log('it workds');
-  table = document.querySelector('#animal-table');
-  table.innerHTML = ""
-
-
-
+  tableBody = document.querySelector('#table-body');
+  tableBody.innerHTML = ''
 };
+
+const populateNewRow = function(event) {
+
+  const name = document.createElement('td');
+  const subgroup = document.createElement('td');
+  const species = document.createElement('td');
+  const country = document.createElement('td');
+  const risk = document.createElement('td');
+  const population = document.createElement('td');
+
+  name.textContent = event.target.name.value;
+  subgroup.textContent = event.target.subgroup.value;
+  species.textContent = event.target.species.value;
+  country.textContent = event.target.country.value;
+  risk.textContent = event.target.risk.value;
+  population.textContent = event.target.population.value;
+
+  const newRow = document.createElement('tr');
+  newRow.classList.add('animal-entry')
+
+  newRow.appendChild(name);
+  newRow.appendChild(subgroup);
+  newRow.appendChild(species);
+  newRow.appendChild(country);
+  newRow.appendChild(risk);
+  newRow.appendChild(population);
+
+  return newRow;
+
+}
